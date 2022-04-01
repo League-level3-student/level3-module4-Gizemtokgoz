d50702d950c0968a715e1a803806cc67da55150a
League Student <league@iMac-12.attlocal.net>
2022-03-31 18:27:44 -0700

    TextUndoRedo

--- src/_00_IntroToStacks/_02_TextUndoRedo.java
+++ src/_00_IntroToStacks/_02_TextUndoRedo.java
@@ -2,6 +2,7 @@ package _00_IntroToStacks;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Stack;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,6 +12,7 @@ public class _02_TextUndoRedo implements KeyListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
+	Stack<String> rem = new Stack<String>();
 	public static void main(String[] args) {
 		new _02_TextUndoRedo();
 	}
@@ -48,7 +50,22 @@ public class _02_TextUndoRedo implements KeyListener {
 		// TODO Auto-generated method stub
 		char kc = e.getKeyChar();
 		System.out.println(kc);
-		label.add
+		
+		
+		System.out.println(e.getKeyCode());
+		
+		if (e.getKeyCode() == 8) {
+			rem.push("" + label.getText().charAt(label.getText().length()-1));
+			label.setText(label.getText().substring(0, label.getText().length()-1));
+		}
+		
+		if (e.getKeyCode() == 81) {
+			label.setText(label.getText() + rem.pop());
+		}
+		
+		else if(e.getKeyCode() != 8){
+			label.setText(label.getText() + kc);
+		}
 	}
 
 	@Override
